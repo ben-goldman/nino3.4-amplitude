7bd203157fc065879fa8d78cba78da547ead59a0
Ben Goldman <bngldmn@gmail.com>
2021-07-20 21:11:54 -0400

    new r stuff outlined presentation yay

--- data/Code_old/extract.sh
+++ data/Code_old/extract.sh
@@ -7,18 +7,6 @@ set -euo pipefail
 
 nces -d time,840,1871 "b.e11.B20TRC5CNBDRD.f09_g16.001.cam.h0.TREFHT.185001-200512.nc" "b.e11.B20TRC5CNBDRD.f09_g16.001.cam.h0.TREFHT.192001-200512.nc"
 
-# ensemble concatenate cesm1 trefht members *DEPRECATED*
-
-ls | grep "b.e11.B20TRC5CNBDRD.f09_g16.\d\{3\}.cam.h0.TREFHT.192001-200512.nc" | ncecat -D 2 -o "b.e11.B20TRC5CNBDRD.f09_g16.all.cam.h0.TREFHT.192001-200512.nc"
-ls | grep "b.e11.BRCP85C5CNBDRD.f09_g16.\d\{3\}.cam.h0.TREFHT.200601-208012.nc" | ncecat -D 2 -o "b.e11.BRCP85C5CNBDRD.f09_g16.001-033.cam.h0.TREFHT.200601-208012.nc"
-ls | grep "b.e11.BRCP85C5CNBDRD.f09_g16.\d\{3\}.cam.h0.TREFHT.208101-210012.nc" | ncecat -D 2 -o "b.e11.BRCP85C5CNBDRD.f09_g16.001-033.cam.h0.TREFHT.208101-210012.nc"
-ls | grep "b.e11.BRCP85C5CNBDRD.f09_g16.\d\{3\}.cam.h0.TREFHT.200601-210012.nc" | ncecat -D 2 -o "b.e11.BRCP85C5CNBDRD.f09_g16.034-105.cam.h0.TREFHT.200601-210012.nc"
-
-# record concatenate 2006-2080 and 2080-2100 files *DEPRECATED*
-
-ls | grep "b.e11.BRCP85C5CNBDRD.f09_g16.001-033.cam.h0.TREFHT.\d\{6\}-\d\{6\}.nc" | ncrcat -D 2 -o "b.e11.BRCP85C5CNBDRD.f09_g16.001-033.cam.h0.TREFHT.200601-210012.nc"
-ncecat -D 2 "b.e11.BRCP85C5CNBDRD.f09_g16.001-033.cam.h0.TREFHT.200601-210012.nc" "b.e11.BRCP85C5CNBDRD.f09_g16.034-105.cam.h0.TREFHT.200601-210012.nc" "b.e11.BRCP85C5CNBDRD.f09_g16.all.cam.h0.TREFHT.200601-210012.nc"
-
 # Take 2
 
 for i in {001..033};
@@ -187,3 +175,42 @@ bits=("1001.001"
  done
 
  ls | grep "BOTH" | ncecat -D 2 -o "b.e21.BOTH.f09_g17.LE2-XXXX.all.cam.h0.TREFHT.185001-210012.nc"
+
+
+ # CESM TEMPDT
+ #
+ #
+
+# 1850 ctl
+ls | grep "b.e11.B1850C5CN.f09_g16.005.pop.h.TEMPDT.\d\{6\}-\d\{6\}.nc" | ncrcat -D 2 -o "b.e11.B1850C5CN.f09_g16.005.pop.h.TEMPDT.040001-220012.nc"
+
+# clip 1850-2005 member at 1920
+nces -d time,840,1871 "b.e11.B20TRC5CNBDRD.f09_g16.001.pop.h.TEMPDT.185001-200512.nc" "b.e11.B20TRC5CNBDRD.f09_g16.001.pop.h.TEMPDT.192001-200512.nc"
+
+# Take 2
+
+for i in {001..033};
+do
+    f1="b.e11.B20TRC5CNBDRD.f09_g16.$i.pop.h.TEMPDT.192001-200512.nc";
+    f2="b.e11.BRCP85C5CNBDRD.f09_g16.$i.pop.h.TEMPDT.200601-208012.nc";
+    f3="b.e11.BRCP85C5CNBDRD.f09_g16.$i.pop.h.TEMPDT.208101-210012.nc";
+    echo $i
+    ncrcat $f1 $f2 $f3 -D 2 -o "b.e11.BRCP85C5CNBDRD.f09_g16.$i.pop.h.TEMPDT.192001-210012.nc";
+done
+
+for i in {034,035,101,102,103,104,105};
+do
+    f1="b.e11.B20TRC5CNBDRD.f09_g16.$i.pop.h.TEMPDT.192001-200512.nc";
+    f2="b.e11.BRCP85C5CNBDRD.f09_g16.$i.pop.h.TEMPDT.200601-210012.nc";
+    ncrcat $f1 $f2 -D 2 -o "b.e11.BRCP85C5CNBDRD.f09_g16.$i.pop.h.TEMPDT.192001-210012.nc";
+done
+
+ncrcat b.e11.B20TRC5CNBDRD.f09_g16.035.pop.h.TEMPDT.192001-200512.nc b.e11.BRCP85C5CNBDRD.f09_g16.035.pop.h.TEMPDT.200601-210012.nc -D 3 -o b.e11.BRCP85C5CNBDRD.f09_g16.035.pop.h.TEMPDT.192001-210012.nc
+
+# Try this to check stuff
+for f in $(ls | grep "192001-210012.nc$"); do echo $f; ncdump -h $f | grep "currently"; echo ""; done
+for f in $(ls | grep "192001-208012.nc$"); do echo $f; ncdump -h $f | grep "currently"; echo ""; done
+
+# Last step for cesm1 trefht
+
+ls | grep "b.e11.BRCP85C5CNBDRD.f09_g16.\d\{3\}.pop.h.TEMPDT.192001-210012.nc" | ncecat -D 3 -o "b.e11.BRCP85C5CNBDRD.f09_g16.all.pop.h.TEMPDT.192001-210012.nc"
