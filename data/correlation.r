eaf6be7bc4c2ca43f945723d28945314c74ff21f
Ben Goldman <bngldmn@gmail.com>
2021-08-10 22:55:30 -0400

    very good pictures for presentation

--- data/correlation.r
+++ data/correlation.r
@@ -4,3 +4,16 @@ library(dplyr)
 library(ggplot2)
 library(WaveletComp)
 library(reshape2)
+
+ncin <- nc_open("/Volumes/Extreme SSD/DATA/stacked/CESM1/TEMP/b.e11.BRCP85C5CNBDRD.f09_g16.all.pop.h.TEMPDT.192001-210012.nc")
+tempdt <- ncvar_get(ncin, "TEMPDT", start = c(21, 1, 1, 1), count = c(121, -1, -1, 1))
+lon <- ncvar_get(ncin, "lon", start = 21, count = 121)
+depth <- ncvar_get(ncin, "depth") / 100
+date <- ncvar_get(ncin, "time") / 365
+
+woohoo <- ncvar_get(ncin, "TEMPDT", start = c(21, 1, 1, 1), count = c(1, 1, -1, -1))
+
+dimnames(tempdt) <- list(lon, depth, date)
+
+ggplot(tempdt[,,4] %>% melt, aes(Var1, log(Var2), z = value)) +
+    geom_contour_filled(bins = 16)
---
+++ data/intro_fig.r
@@ -0,0 +1,99 @@
+library(ncdf4)
+library(zoo)
+library(dplyr)
+library(ggplot2)
+library(reshape2)
+library(scico)
+
+ncin <- nc_open("/Volumes/Extreme SSD/DATA/HadISST_sst.nc")
+
+sst <- ncvar_get(ncin, "sst")
+sst[sst == -1000] <- NA
+
+date <- ncvar_get(ncin, "time")
+date <- date/365 + 1870
+
+lat <- ncvar_get(ncin, "latitude")
+lon <- ncvar_get(ncin, "longitude")
+
+rownames(sst) <- lon
+colnames(sst) <- lat
+
+sst_wrap <- array(NA, dim = dim(sst))
+## lon_wrap <- rep(NA, times = length(lon))
+## lon_wrap[0:211] <- lon[150:360]
+## lon_wrap[212:360] <- lon[1:149]
+lon_wrap <- lon + 180
+sst_wrap[0:180,,] <- sst[181:360,,]
+sst_wrap[181:360,,] <- sst[1:180,,]
+rownames(sst_wrap) <- lon_wrap
+colnames(sst_wrap) <- lat
+
+## NEED TO MAKE ANOMALY PLOT!
+monthly_avgs <- array(NA, dim = c(360, 180, 12))
+
+for (i in c(1:12)) {
+    writeLines(as.character(i))
+    monthly_avgs[,,i] <- sst_wrap[,,seq(i, 1800 + i, 12)] %>% apply(c(1,2), mean, na.rm = TRUE)
+}
+
+rownames(monthly_avgs) <- lon_wrap
+colnames(monthly_avgs) <- lat
+
+monthly_avgs_melt <- monthly_avgs %>% melt(varnames = c("longitude", "latitude", "month"))
+
+monthly_avgs_melt$month <- as.factor(monthly_avgs_melt$month)
+# levels = c("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Sep", "Oct", "Nov", "Dec"))
+
+sst_anom <- array(NA, dim = dim(sst_wrap))
+months <- rep(1:12, length.out = dim(sst_wrap)[3])
+
+for(t in 1:dim(sst_wrap)[3]) {
+    sst_anom[,,t] <- sst_wrap[,,t] - monthly_avgs[,,months[t]]
+}
+dimnames(sst_anom) <- list(lon_wrap, lat, date)
+
+
+
+sst_anom_1997_melt <- sst_anom[,,match(date[abs(date - 1997.75) < 1], date)] %>%
+    apply(c(1,2), mean) %>%
+    melt(varnames = c("longitude", "latitude"))
+
+sst_anom_1975_melt <- sst_anom[,,match(date[abs(date - 1975.75) < 1], date)] %>%
+    apply(c(1,2), mean) %>%
+    melt(varnames = c("longitude", "latitude"))
+
+sst_anom_compare <- sst_anom_1997_melt %>% mutate(case = "1997 El Niño") %>%
+    bind_rows(sst_anom_1975_melt %>% mutate(case = "1975 La Niña"))
+
+gsave <- function(name, plot = last_plot(), dimensions = c(6, 8), path = "./figures/") {
+    ggsave(name, path = path, width = dimensions[1], height = dimensions[2])
+}
+
+ggplot(sst_anom_compare %>% filter(latitude < 60 &
+                           latitude > -60 &
+                           longitude > 50 &
+                           longitude < 350), aes(longitude, latitude, fill = value)) +
+    geom_raster() +
+    facet_wrap(vars(case), nrow = 2, ncol = 1) +
+    scale_fill_scico(palette = "vik") +
+    labs(title = "Pacific SST Anomalies Comparison",
+         x = "Longitude",
+         y = "Latitude",
+         fill = " SST \n Anomaly \n (°C)")
+
+gsave("intro_fig.pdf")
+
+ggplot(sst_anom_compare %>% filter(latitude < 60 &
+                           latitude > -60 &
+                           longitude > 50 &
+                           longitude < 350), aes(longitude, latitude, z = value)) +
+    geom_contour_filled(bins = 20) +
+    facet_wrap(vars(case), nrow = 2, ncol = 1) +
+    scale_fill_scico_d(palette = "vik") +
+    labs(title = "Pacific SST Anomalies Comparison",
+         x = "Longitude",
+         y = "Latitude",
+         fill = " SST \n Anomaly \n (°C)")
+
+gsave("intro_fig_2.pdf")
