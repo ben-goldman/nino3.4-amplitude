b98fa9ec77cc35927a7c23c0a09e10ba81135c00
Ben Goldman <bngldmn@gmail.com>
2021-08-16 17:16:40 -0400

    TEMPDT correlation plot!

--- data/correlation.r
+++ data/correlation.r
@@ -5,15 +5,108 @@ library(ggplot2)
 library(WaveletComp)
 library(reshape2)
 
+# subtract ensemble mean from member
+prepare <- function(x) {
+    x.mean <- x %>% apply(1, mean)
+    return(x - x.mean)
+}
+
+# calculate 20-year rolling variance
+rollvar <- function(x) {
+  out <- rollapply(x, width = 240, FUN = var, align = "center", fill = NA)
+  return(out)
+}
+
+# load variance from ensemble file
+load_variance <- function(file) (read.table(file, sep = ",") %>% as.matrix %>% apply(2, rollvar))
+
+se <- function(x, ...) sd(x, ...)/sqrt(length(x))
+
+# bind ensemble statistics into dataframe while preserving date offset
+bind_stats <- function(this.date, this.mean, this.se, date.start = NA, date.end = NA) {
+    if(length(this.mean) == length(this.date)) {
+        this.df <- data.frame(date = this.date, mean = this.mean, se = this.se)
+    } else {
+        if(is.na(date.end)){
+            date.end <- length(this.date)
+        }
+        if(is.na(date.start)){
+            date.start <- 0
+        }
+        this.df <- data.frame(date = this.date, mean = c(
+                                                    rep(NA, date.start),
+                                                    this.mean,
+                                                    rep(NA, (length(this.date) - date.end))),
+                              se =
+                                  c(rep(NA, date.start),
+                                    this.se,
+                                    rep(NA, (length(this.date) - date.end))))
+        writeLines(as.character(date.start))
+        writeLines(as.character(date.end))
+        return(this.df)
+    }
+}
+
+# Produce ensemble stats dataframe
+make_df <- function(this, this.date, date.start = NA, date.end = NA) {
+    this.mean <- this %>% apply(1, mean)
+    this.se <- this %>% apply(1, se)
+    writeLines(as.character(length(this.mean)))
+    this.df <- bind_stats(this.date, this.mean, this.se, date.start, date.end)
+    return(this.df)
+}
+
+na_cor <- function(x, y, ...) {
+    if(all(is.na(x)) || all(is.na(y))) {
+        return(NA)
+    } else {
+        return(cor(x, y, ...))
+    }
+}
+
 ncin <- nc_open("/Volumes/Extreme SSD/DATA/stacked/CESM1/TEMP/b.e11.BRCP85C5CNBDRD.f09_g16.all.pop.h.TEMPDT.192001-210012.nc")
-tempdt <- ncvar_get(ncin, "TEMPDT", start = c(21, 1, 1, 1), count = c(121, -1, -1, 1))
 lon <- ncvar_get(ncin, "lon", start = 21, count = 121)
 depth <- ncvar_get(ncin, "depth") / 100
 date <- ncvar_get(ncin, "time") / 365
 
-woohoo <- ncvar_get(ncin, "TEMPDT", start = c(21, 1, 1, 1), count = c(1, 1, -1, -1))
+tempdt <- ncvar_get(ncin, "TEMPDT", start = c(21, 1, 1, 1), count = c(121, -1, -1, 40), verbose = TRUE)
 
-dimnames(tempdt) <- list(lon, depth, date)
+t_melt <- tempdt[,,1,1] %>% melt
 
-ggplot(tempdt[,,4] %>% melt, aes(Var1, log(Var2), z = value)) +
+record <- c(1:40)
+
+dimnames(tempdt) <- list(lon, depth, date, record)
+
+ggplot(tempdt[,,1,1] %>% melt, aes(Var1, Var2, z = value)) +
     geom_contour_filled(bins = 16)
+
+nino_var <- load_variance("./data/CESM1/ff.csv")
+
+corrs <- array(NA, dim = dim(tempdt)[c(1, 2, 4)])
+for(r in 1:dim(tempdt)[4]) {
+    corrs[,,r] <- apply(X = tempdt[,,,r],
+                        MARGIN = c(1, 2),
+                        FUN=na_cor,
+                        y = nino_var[,r],
+                        use = "complete.obs")
+    writeLines(as.character(r))
+}
+
+dimnames(corrs) <- list(lon, depth, record)
+
+corrs_mean <- apply(corrs, c(1, 2), mean)
+dimnames(corrs_mean) <- list(lon, depth)
+
+ggplot(corrs_mean %>% melt, aes(Var1, log(Var2, base = 10), z = value)) +
+    geom_contour_filled(bins = 20) +
+    labs(title = "Correlation Coefficient Between Niño 3.4 Variance and Ocean Temperature",
+         x = "Longitude",
+         y = "log_10(Depth)",
+         fill = "Correlation\nCoefficient") +
+    scale_y_continuous(trans = "reverse")
+
+gsave <- function(name, plot = last_plot(), dimensions = c(6, 8), path = "./figures/") {
+    ggsave(name, path = path, width = dimensions[1], height = dimensions[2])
+}
+
+gsave("tempdt.pdf", dimensions = c(8, 6))
